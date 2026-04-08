2a4db0522787ec10e8a130b35353c4b06d3ee17c
edwinges-max <super8585super@gmail.com>
2026-04-07 20:50:06 -0500

    Convertir proyecto en pipeline Snakemake reproducible

--- scripts/resumen.py
+++ scripts/resumen.py
@@ -1,3 +1,8 @@
-with open("resultados/exp01/salida.tsv", "w") as f:
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(output_file, "w") as f:
     f.write("id\tlongitud\n")
     f.write("seq1\t100\n")
